c8c802caf6608d3fa33a69bdc864e8ba8478a367
Julia <up.doroshina@yandex.ru>
2022-08-09 23:01:51 +0300

    type vs. interface

---
+++ advanced_types/type_vs_interface.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+// type User = {} // error -- type так не умеет, будет ругаться, т.к. User уже определен
+var user = {
+    name: 'qweqwe',
+    age: 12
+};
